6a1596bba399200fa9e7a8485195166f5b963db4
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-03-20 08:18:54 -0300

    Atualizando

---
+++ index.js
@@ -0,0 +1,23 @@
+var ar = ["R", "O", "M"];
+
+var times = 8;
+
+function addPositions(timesToDo) {
+	let times = timesToDo;
+
+	for (t = 0; t <= times; t++) {
+		console.log("Ativado essa vez.");
+	}
+
+	return times;
+}
+
+// for (t = 1; t <= times; t++) {
+// 	console.log(`Ativado essa vez ${t}.`);
+// }
+
+// console.log(addPositions(3));
+
+// for (t = times; (t = 0); t--) {
+// 	console.log("Ativado essa vez.");
+// }
