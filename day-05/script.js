7a86a4f86e112792def36480ec46b94c5d826a03
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-04-05 23:05:00 -0300

    Atualizando

---
+++ day-05/script.js
@@ -0,0 +1,19 @@
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+	load++;
+	if (load > 99) {
+		clearInterval(int);
+	}
+
+	console.log(load);
+
+	loadText.innerText = `${load}%`;
+	loadText.style.opacity = `${100 - load}%`;
+	bg.style.filter = `blur(${100 - load}px)`;
+}
