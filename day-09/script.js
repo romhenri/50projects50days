ec4e5f26d77efa6f50b717a823d7defebb38386f
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-04-09 21:51:45 -0300

    Atualizando

---
+++ day-09/script.js
@@ -0,0 +1,14 @@
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+sounds.forEach((sound) => {
+	const btn = document.createElement("button");
+	btn.classList.add("btn");
+
+	btn.innerText = sound;
+
+	btn.addEventListener("click", () => {
+		document.getElementById(sound).play();
+	});
+
+	document.getElementById("buttons").appendChild(btn);
+});
