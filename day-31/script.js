657f353564cba6468003dde966a0630de2e5804e
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-05-12 20:11:10 -0300

    fix: day-31

--- day-31/script.js
+++ day-31/script.js
@@ -7,8 +7,36 @@ const elmSymbols = document.getElementById("inputSymbols");
 const btnGenerate = document.getElementById("btnGenerate");
 const elmClipboard = document.getElementById("clipboard");
 
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol,
+};
+
+console.log(randomFunc);
+
+// console.log(`generatePassword: ${generatedPassword}`);
+
+elmClipboard.addEventListener("click", () => {
+	const textarea = document.createElement("textarea");
+	const password = resultEl.innerText;
+
+	if (!password) {
+		return;
+	}
+
+	textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand("copy");
+	alert("copied!");
+});
+
 btnGenerate.addEventListener("click", () => {
-	const length = +elmLength.value;
+	console.log("Botão Ativo");
+
+	const length = elmLength.value;
 	const hasLower = elmLower.checked;
 	const hasUpper = elmUpper.checked;
 	const hasNumber = elmNumber.checked;
@@ -23,10 +51,12 @@ btnGenerate.addEventListener("click", () => {
 		hasSymbol,
 		length
 	);
+	// console.log(`generatePassword: ${generatedPassword}`);
 });
 
 function generatePassword(lower, upper, number, symbol, length) {
 	let generatedPassword = "";
+
 	const typesCount = lower + upper + number + symbol;
 	const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
 		(item) => Object.values(item)[0]
@@ -48,13 +78,6 @@ function generatePassword(lower, upper, number, symbol, length) {
 	return finalPassword;
 }
 
-const randomFunc = {
-	lower: getRandomLower,
-	upper: getRandomUpper,
-	number: getRandomNumber,
-	symbol: getRandomSymbol,
-};
-
 function getRandomLower() {
 	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -71,5 +94,3 @@ function getRandomSymbol() {
 	const symbols = "!@#$%^&*()_+-={}[]:\";'<>?,.";
 	return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
-// console.log(getRandomLower());
